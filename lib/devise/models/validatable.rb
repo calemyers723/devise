3ad596ebe540ac52bcbf97b8f799d6af8a4fd8a2
calemyers723 <cale.myers723@gmail.com>
2018-01-27 09:54:40 +0800

    email uniquness with scope

--- lib/devise/models/validatable.rb
+++ lib/devise/models/validatable.rb
@@ -28,10 +28,10 @@ module Devise
         base.class_eval do
           validates_presence_of   :email, if: :email_required?
           if Devise.activerecord51?
-            validates_uniqueness_of :email, allow_blank: true, if: :will_save_change_to_email?
+            validates_uniqueness_of :email, scope: authentication_keys[1..-1], allow_blank: true, if: :will_save_change_to_email?
             validates_format_of     :email, with: email_regexp, allow_blank: true, if: :will_save_change_to_email?
           else
-            validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+            validates_uniqueness_of :email, scope: authentication_keys[1..-1], allow_blank: true, if: :email_changed?
             validates_format_of     :email, with: email_regexp, allow_blank: true, if: :email_changed?
           end
 
